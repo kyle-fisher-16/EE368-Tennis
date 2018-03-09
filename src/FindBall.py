1a01089fbfe4d24e6d893f1ebb57506c32864ff7
fujinam2 <tfujinami@ymail.com>
2018-03-09 14:38:22 -0800

    still trying random stuff to make ball finding more robust, but structure back to more readable and consistent

--- src/FindBall.py
+++ src/FindBall.py
@@ -16,13 +16,13 @@ class BallFinder(object):
     def __init__(self):
         # hsv in opencv: h [0,179], s [0,255], v [0,255]
         # self.ballColHsv = np.array([int(44), int(115), int(240)])
-        self.ballColHsv = np.array([int(44), 127, int(240)])
+        self.ballColHsv = np.array([int(44), 127, int(230)])
         # far away balls have very small saturation levels
         satThresh = 127;
-        valThresh = 15;
-        hueThresh = 30;
-        self.lowerCol = self.ballColHsv - [hueThresh, satThresh, valThresh]
-        self.upperCol = self.ballColHsv + [hueThresh, satThresh, valThresh]
+        valThresh = 25;
+        hueThresh = 40;
+        self.lowerCol = np.array([self.ballColHsv[0]-hueThresh, 0, 220])
+        self.upperCol = np.array([self.ballColHsv[0]+hueThresh, 255, 255])
         # centroid of found ball in motion for tracking
         self.ballPixelLoc = [0,0]
         # maybe we should try like kalman filtering this or something?
@@ -32,21 +32,55 @@ class BallFinder(object):
     # set withFilt to False if don't want to perform opening to get rid of noise
         # if ball is far away it is very small and
         # may be smaller than noise in foreground
-    def maskDiff(self, frame1, frame2, withFilt=True):
+    def hsvDiff(self, frame1, frame2, withFilt=True):
         hsvFrame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
         hsvFrame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
-        mask1 = cv2.inRange(hsvFrame1, self.lowerCol, self.upperCol)
-        mask2 = cv2.inRange(hsvFrame2, self.lowerCol, self.upperCol)
+        # hsv narrow range:
+        hue = self.ballColHsv[0]
+        hueThresh = 15
+        lowerCol = np.array([hue-hueThresh, self.lowerCol[1], self.lowerCol[2]])
+        upperCol = np.array([hue+hueThresh, self.upperCol[1], self.upperCol[2]])
+        mask1 = cv2.inRange(hsvFrame1, lowerCol, upperCol)
+        mask2 = cv2.inRange(hsvFrame2, lowerCol, upperCol)
         # remember that these are uints, so no negative values
         # xor to get 2 distinct balls, then and with original to only get first ball
         frameDiff = mask1 ^ mask2
         frameBall1 = frameDiff & mask1
         if withFilt:
-            se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+            se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
             frameBall1Filt = cv2.morphologyEx(frameBall1, cv2.MORPH_OPEN, se)
             return frameBall1Filt
         else:
             return frameBall1
+
+    def rgbDiff(self, frame1, frame2):
+        frameDiff = frame1.astype(np.int16) - frame2.astype(np.int16)
+        frameDiff = abs(frameDiff)
+        frameDiff = frameDiff.astype(np.uint8)
+        # threshold difference
+        frameDiffGray = cv2.cvtColor(frameDiff, cv2.COLOR_BGR2GRAY)
+        mask = cv2.inRange(frameDiffGray, 50, 255)
+        se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
+        maskFilt = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se)
+        se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+        maskFilt = cv2.morphologyEx(maskFilt, cv2.MORPH_DILATE, se)
+        # add filter by eccenetricity to get rid of too big and components etc
+        ___notsure, contours, hier = cv2.findContours(maskFilt,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+        maskEcc = np.zeros(maskFilt.shape, dtype="uint8");
+        for c in contours:
+            cArea = cv2.contourArea(c)
+            if len(c) < 5: # too small to measure eccenetricity
+                cv2.drawContours(maskEcc,[c],0,255,-1)
+                continue;
+            c_ellipse = cv2.fitEllipse(c)
+            rect = c_ellipse[1];
+            ecc = np.max([rect[0]/(rect[1]+0.001), rect[1]/(rect[0]+0.001)]);
+            if ecc < 2 and cArea < 100:
+                cv2.drawContours(maskEcc,[c],0,255,-1)
+
+        colorFiltMask = self.hsvFilt(frame1, False)
+        maskFilt = colorFiltMask & maskEcc
+        return maskFilt
 
     # returns the binary frame filtered in HSV space where filtered for ball color
     def hsvFilt(self, frame, withFilt=True):
@@ -58,6 +92,38 @@ class BallFinder(object):
             return frameBallFilt
         else:
             return ballFrame
+
+    # GetCornernessMask(frame, thresh, dilateSz)
+    #    frame: RGB image
+    #    thresh: lower threshold for corner to be registered
+    #            set to 0.0 to register everything
+    #            set to 1.0 to register nothing
+    #    dilateSz: size to dilate the corners so they make a useful mask
+    def GetCornernessMask(self, frame1, frame2, thresh=0.01, dilateSz=6):
+        frameDiff = frame1.astype(np.int16) - frame2.astype(np.int16)
+        frameDiff = abs(frameDiff)
+        frameDiff = frameDiff.astype(np.uint8)
+        # threshold difference
+        gray = cv2.cvtColor(frameDiff, cv2.COLOR_BGR2GRAY)
+        colorFiltMask = self.hsvFilt(frame1, False)
+        gray = gray & colorFiltMask
+        # gray = cv2.cvtColor(frameIn,cv2.COLOR_BGR2GRAY)
+        gray = np.float32(gray);
+        dst = cv2.cornerHarris(gray,2,3,0.04);
+        dst = cv2.dilate(dst,None);
+        cornersMask = np.zeros(gray.shape, dtype="uint8");
+        cornersMask[dst>thresh*dst.max()] = 255;
+        se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(dilateSz,dilateSz))
+        cornersMask = cv2.morphologyEx(cornersMask, cv2.MORPH_DILATE, se)
+        return cornersMask;
+
+    def aveMask(self, mask1, mask2, mask3):
+        sumMask = np.zeros(mask1.shape, dtype="int32")
+        sumMask = mask1.astype(np.int32) + mask2.astype(np.int32) + mask3.astype(np.int32)
+        aveMask = sumMask/3;
+        aveMask = aveMask.astype(np.uint8)
+        mask = cv2.inRange(aveMask, int(2*255/3), 255)
+        return mask
 
     # calculate the centroid of a ball where frame_mask is a binary frame
     # sets self.ballPixelLoc and returns if ball was found or not
--- src/main.py
+++ src/main.py
@@ -20,100 +20,54 @@ def calcAvgFrame(frames):
 
 def main():
 
-    filename = '../UntrackedFiles/angle3_5.mp4'
+    filename = '../UntrackedFiles/stereoClip5_Megan.mov'
     vr = VideoReader(filename)
     # find court corners:
     cf = CourtFinder()
-    numFrames = vr.getNumFrames()
+    numFrames = int(vr.getNumFrames())
     vr.setNextFrame(int(numFrames/2))
     ret, frame = vr.readFrame()
     cf.FindCourtCorners(frame, 0)
 
     # reset frame index to beginning
     vr.setNextFrame(0)
-    # having 2 video readers is much much faster than resetting next frame id
-    vr2 = VideoReader(filename)
     bf = BallFinder()
 
-    numFrames = int(vr.getNumFrames())
-    vr2.setNextFrame(5) # for background subtraction, 5 frames ahead-ish
+    # second video reader for faster processing
+    vr2 = VideoReader(filename)
+    numFrameForward = 5
+    vr2.setNextFrame(numFrameForward)
 
-    # initialize
-    numVRs = 5;
-    vrs = [VideoReader(filename) for i in range(0,numVRs)];
-    for i in range(0,numVRs):
-        vrs[i].setNextFrame(i);
-
+    # # calculate average frame for background
+    # numFrameForAve = 10;
+    # ret, sumFrame = vr.readFrame()
+    # sumFrame = sumFrame.astype(np.int16)
+    # for i in range(1,numFrameForAve):
+    #     ret, frame = vr.readFrame()
+    #     sumFrame += frame.astype(np.int16)
+    # avgFrame = sumFrame/numFrameForAve
+    # avgFrame = avgFrame.astype(np.uint8)
+    #
+    # # reset video reader index to beginning again
+    # vr.setNextFrame(0)
 
     done = False
     while(not(done)):
-        rets = [];
-        frames = [];
-        for i in range(0, numVRs):
-            ret, frame = vrs[i].readFrame();
-            if not(ret):
-                done = True
-            print 'FRAME SHAPE', frame.shape
-            frames.append(frame);
-            rets.append(frame);
-
-        avgFrame = calcAvgFrame(frames);
-
-
-        # trying some stuff to make ball finding more robust
-        # absolute frame difference:
-        frameDiff = frames[0].astype(np.int16) - avgFrame.astype(np.int16)
-        frameDiff = abs(frameDiff)
-        frameDiff = frameDiff.astype(np.uint8)
-        # threshold difference
-        frameDiff = cv2.cvtColor(frameDiff, cv2.COLOR_BGR2GRAY)
-        mask = cv2.inRange(frameDiff, 50, 255)
-        se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
-        mask_filt = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se)
-
-        se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-        mask_filt = cv2.morphologyEx(mask_filt, cv2.MORPH_DILATE, se)
-        ___notsure, contours, hier = cv2.findContours(mask_filt,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-        mask_ecc = np.zeros(mask_filt.shape, dtype="uint8");
-        for c in contours:
-            cArea = cv2.contourArea(c)
-            if len(c) < 5: # too small to measure eccenetricity
-                cv2.drawContours(mask_ecc,[c],0,255,-1)
-                continue;
-            c_ellipse = cv2.fitEllipse(c)
-            rect = c_ellipse[1];
-            ecc = np.max([rect[0]/(rect[1]+0.001), rect[1]/(rect[0]+0.001)]);
-            if ecc < 2 and cArea < 150:
-                cv2.drawContours(mask_ecc,[c],0,255,-1)
-
-        colorFiltMask = bf.hsvFilt(frames[0], False);
-        mask_filt = colorFiltMask & mask_filt;
-
-        # connectedComps = cv2.connectedComponentsWithStats(mask, 8)
-        # stats = connectedComps[2]
-        # print 'stats ', stats
-        frameFilt = frames[0] & np.dstack((mask,mask,mask))
-        cv2.imshow('frameDiff',cv2.resize(mask_filt, (960, 540)))
-        #cv2.imshow('frameDiff', frameDiff)
-        cv2.waitKey(1)
-
-
-
-        #print "press q enter to quit "
-        #done = False
-        #while(not(done)):
-        #    c = sys.stdin.read(1)
-        #    if c == 'q':
-        #       done = True
-        #return
-
-        # frameDiff = bf.maskDiff(frame, frame2, False)
-        # findBall = bf.calcBallCenter(frameDiff)
-        # frame = bf.drawBallOnFrame(frame)
-        # if cf.found_corners:
-        #     frame = cf.drawCornersOnFrame(frame)
-        # cv2.imshow('frame',cv2.resize(frameDiff, (960, 540)))
-        # cv2.waitKey(1)
+        ret, frame = vr.readFrame()
+        ret2, frame2, = vr2.readFrame()
+        if not(ret) or not(ret2):
+            done = True
+        else:
+            frameDiff1 = bf.hsvDiff(frame, frame2)
+            frameDiff2 = bf.rgbDiff(frame, frame2)
+            frameCornerMask = bf.GetCornernessMask(frame, frame2)
+            mask = bf.aveMask(frameDiff1, frameDiff2, frameCornerMask)
+            findBall = bf.calcBallCenter(mask)
+            frame = bf.drawBallOnFrame(frame)
+            if cf.found_corners:
+                frame = cf.drawCornersOnFrame(frame)
+            cv2.imshow('frame',cv2.resize(mask, (960, 540)))
+            cv2.waitKey(1)
 
     # cv2.imwrite('../UntrackedFiles/frame_diff.jpg', frame_diff)
     # # quit sequence:
