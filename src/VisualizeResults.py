dcde3cb00afdad2719dba10361635b3feea23ced
Megan Fazio <mefazio@stanford.edu>
2018-03-11 20:14:50 -0700

    Updated visualization code

--- src/VisualizeResults.py
+++ src/VisualizeResults.py
@@ -119,14 +119,14 @@ def main():
     ax1 = fig.add_subplot(2,2,1)
     ax1.xaxis.set_visible(False)
     ax1.yaxis.set_visible(False)
-    ax1.set_title("Megan's Camera")
+    ax1.set_title("iPhone Camera #1")
     im1 = ax1.imshow(frame1)
     for x, y in corners1:
         ax1.scatter(x, y, s=16, c='red', marker='o')
     ax2 = fig.add_subplot(2,2,2)
     ax2.xaxis.set_visible(False)
     ax2.yaxis.set_visible(False)
-    ax2.set_title("Kyle's Camera")
+    ax2.set_title("iPhone Camera #2")
     im2 = ax2.imshow(frame2)
     for x, y in corners2:
         ax2.scatter(x, y, s=16, c='red', marker='o')
@@ -152,17 +152,65 @@ def main():
     ax4.set_zlabel("Ball Height (m)")
     ax4.set_aspect('equal')
 
-    plt.show()
+    fig.show()
 
     # output video file
     imFilename = '../UntrackedFiles/imageOutput/image' + str(currentFrame) + '.png'
-    plt.savefig(imFilename)
+    fig.set_size_inches(12, 8)
+    plt.savefig(imFilename, dpi=200)
     # vidFrame = cv.imread(imFilename)
     # vidFrameHeight, vidFrameWidth, _ = vidFrame.shape
     # outFilename = '../UntrackedFiles/testVideo.mp4'
     # fourcc = cv.VideoWriter_fourcc(*'mp4v');
     # outputVideo = cv.VideoWriter(outFilename,fourcc, 60, (vidFrameWidth,vidFrameHeight));
     # outputVideo.write(vidFrame)
+
+    # for poster
+    # Megan's camera
+    fig1 = plt.figure(figsize=(12, 8))
+    fig1ax1 = fig1.add_subplot(1, 1, 1)
+    fig1ax1.xaxis.set_visible(False)
+    fig1ax1.yaxis.set_visible(False)
+    fig1ax1.set_title("iPhone Camera #1")
+    fim1 = fig1ax1.imshow(frame1)
+    for x, y in corners1:
+        fig1ax1.scatter(x, y, s=16, c='red', marker='o')
+    fig1.show()
+
+    # Kyle's Camera
+    fig2 = plt.figure(figsize=(12, 8))
+    fig2ax1 = fig2.add_subplot(1, 1, 1)
+    fig2ax1.xaxis.set_visible(False)
+    fig2ax1.yaxis.set_visible(False)
+    fig2ax1.set_title("iPhone Camera #2")
+    fim2 = fig2ax1.imshow(frame2)
+    for x, y in corners2:
+        fig2ax1.scatter(x, y, s=16, c='red', marker='o')
+    fig2.show()
+
+    # court graphic
+    fig3 = plt.figure(figsize=(6, 8))
+    fig3ax1 = fig3.add_subplot(1, 1, 1)
+    fig3ax1.xaxis.set_visible(False)
+    fig3ax1.yaxis.set_visible(False)
+    fig3ax1.imshow(courtTopView)
+    fig3.show()
+
+    # 3D scatterplot
+    fig4 = plt.figure(figsize=(12, 8))
+    fig4ax1 = fig4.add_subplot(1, 1, 1, projection = '3d')
+    fig4ax1.set_xlim([zPosMin, zPosMax])
+    fig4ax1.set_xticks(np.arange(-20, 21, 10))
+    fig4ax1.set_xlabel("Court Length (m)")
+    fig4ax1.set_ylim([xPosMin, xPosMax])
+    fig4ax1.set_yticks(np.arange(-6, 7, 3))
+    fig4ax1.set_ylabel("Court Width (m)")
+    fig4ax1.set_zlim([yPosMin, yPosMax])
+    fig4ax1.set_zticks(np.arange(0, 4, 1))
+    fig4ax1.set_zlabel("Ball Height (m)")
+    fig4ax1.set_aspect('equal')
+    fig4.show()
+
 
     # update plots in real-time
     for f in range(1, numFrames):
@@ -179,21 +227,25 @@ def main():
             [xv, yv, zv] = velocityData[f, :]
             velMPH = np.sqrt((xv * msToMPH) ** 2 + (yv * msToMPH) ** 2 + (zv * msToMPH) ** 2)
             #print 'Velocity: ' + str(velMPH) + ' mph'
-            speedText = 'Speed: ' + str(round(velMPH, 2)) + ' mph'
+            speedText = 'Speed: ' + str(int(round(velMPH))) + ' mph'
             ax3.set_title(speedText)
             #ax3.text(4, 4, speedText, fontsize=12)
 
         # Megan's camera
         im1.set_data(frame1)
+        fim1.set_data(frame1)
         if ballFound[f]:  # plot pixel coordinates of ball
             [x1, y1] = pixelsMegan[f,:]
             ax1.scatter(x1, y1, s=1, c='pink', marker='x')
+            fig1ax1.scatter(x1, y1, s=1, c='pink', marker='x')
 
         # Kyle's camera
         im2.set_data(frame2)
+        fim2.set_data(frame2)
         if ballFound[f]:
             [x2, y2] = pixelsKyle[f,:]
             ax2.scatter(x2, y2, s=1, c='pink', marker='x')
+            fig2ax1.scatter(x2, y2, s=1, c='pink', marker='x')
 
         # court graphic
         if ballFound[f]:
@@ -202,19 +254,42 @@ def main():
                 xc = int(round(((x - xCourtMin) / (2 * xCourtMax)) * courtWidth))
                 yc = int(round(((z - yCourtMin) / (2 * yCourtMax)) * courtHeight))
                 ax3.scatter(xc, yc, s=2, c='pink', marker='o')
-
+                fig3ax1.scatter(xc, yc, s=2, c='pink', marker='o')
 
         # 3D scatterplot
         if ballFound[f]:
             [x, y, z] = positionData[f, :]
             ax4.scatter(z, x, y, s=2, c='blue', marker='o')
+            fig4ax1.scatter(z, x, y, s=2, c='blue', marker='o')
+
+        # graphics for poster
+        if f == 100:
+
+            # Megan's camera
+            fig1.show()
+            imFilename = '../UntrackedFiles/imageOutput/plot1.png'
+            fig1.savefig(imFilename, dpi=200)
+
+            # Kyle's camera
+            fig2.show()
+            imFilename = '../UntrackedFiles/imageOutput/plot2.png'
+            fig2.savefig(imFilename, dpi=200)
+
+            # court graphic
+            fig3.show()
+            imFilename = '../UntrackedFiles/imageOutput/plot3.png'
+            fig3.savefig(imFilename, dpi=200)
+
+            # 3D scatterplot
+            fig4.show()
+            imFilename = '../UntrackedFiles/imageOutput/plot4.png'
+            fig4.savefig(imFilename, dpi=200)
 
         # update plots
-
-        plt.show()
+        fig.show()
         imFilename = '../UntrackedFiles/imageOutput/image' + str(f) + '.png'
         fig.set_size_inches(12, 8)
-        plt.savefig(imFilename, dpi=100)
+        fig.savefig(imFilename, dpi=200)
         #vidFrame = cv.imread(imFilename)
         #outputVideo.write(vidFrame)
         plt.pause(0.00001)
@@ -222,7 +297,7 @@ def main():
     # close video readers
     vr1.close()
     vr2.close()
-    outputVideo.release()
+    #outputVideo.release()
 
 
 
